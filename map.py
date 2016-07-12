0a2b6bd6768546044c1be154393df7168c105ff1
tails128 <klonoa128@gmail.com>
2016-07-12 15:51:31 +0200

    map.py corrections

--- map.py
+++ map.py
@@ -4,8 +4,8 @@ import fileinput
 indexIdMachine = 2
 indexDowntime = 3
 
-	for line in fileinput.input():
-		values = line.split(';')
-			idMachine = values[indexIdMachine]
-			downtime = values[indexDowntime]
-			print(('{0}\t{1}'.format(category, total)).replace("\n",""))
+for line in fileinput.input():
+	values = line.split(';')
+	idMachine = values[indexIdMachine]
+	downtime = values[indexDowntime]
+	print(('{0}\t{1}'.format(category, total)).replace("\n",""))
