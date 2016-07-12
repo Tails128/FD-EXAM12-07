9a8c2c70f8f5382ac34cef2d0b8ca457fc9a44f3
tails128 <klonoa128@gmail.com>
2016-07-12 16:23:56 +0200

    Added comments to the mapper

--- map.py
+++ map.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 import fileinput
 
-indexIdMachine = 2
-indexDowntime = 3
+indexIdMachine = 2		#field IDMachine's position
+indexDowntime = 3		#field downtime's position
 
-for line in fileinput.input():
+for line in fileinput.input():				#read and split of the csv
 	values = line.split(';')
-	idMachine = values[indexIdMachine]
+	idMachine = values[indexIdMachine]		#mapping
 	downtime = values[indexDowntime]
-	print(('{0}\t{1}'.format(idMachine, downtime)).replace("\n",""))
+	print(('{0}\t{1}'.format(idMachine, downtime)).replace("\n",""))  #mapped output
