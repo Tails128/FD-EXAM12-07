8e8bbbce4c747ae6aafb1d7086fd9a3e7b2a3eea
tails128 <klonoa128@gmail.com>
2016-07-12 15:52:51 +0200

    Further corrections to mapper (changed variables names)

--- map.py
+++ map.py
@@ -8,4 +8,4 @@ for line in fileinput.input():
 	values = line.split(';')
 	idMachine = values[indexIdMachine]
 	downtime = values[indexDowntime]
-	print(('{0}\t{1}'.format(category, total)).replace("\n",""))
+	print(('{0}\t{1}'.format(idMachine, downtime)).replace("\n",""))
